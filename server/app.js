4dfb27fd93a284f0e2de604bb9f965463435e223
Mikołaj Szymański <mikszym95@gmail.com>
2018-11-29 23:09:35 +0100

    Add Deeadline route

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ app.use(express.static('public'));
 // Change "Your_GitHub_Name" with your GitHub profile name for ex. MarekDzw
 //Add route require below
 let MarekDzw = require('./routes/MarekDzw');
+const Deeadline = require('./routes/Deeadline');
 
 // Copy the commented line below and paste it under "Add your route below" and uncomment it!
 //app.get('/Your_GitHub_Name', Your_GitHub_Name.profile);
 // Change "Your_GitHub_Name" with your GitHub profile name for ex. MarekDzw
 // Add your route below
 app.get('/MarekDzw', MarekDzw.profile);
+app.get('/Deeadline', Deeadline.profile);
 
 module.exports = app;
